6859d38cf431b0491471100e9bc0d6eedddf692f
Vinicius Lima <vibylima75@gmail.com>
2025-11-14 09:11:35 -0300

    Mudando banco senha

--- backend/models.py
+++ backend/models.py
@@ -14,7 +14,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    senha = db.Column(db.String(100), nullable=False)
+    senha = db.Column(db.String(255), nullable=False)
     creditos = db.Column(db.Integer, default=10000)
     avatar_url = db.Column(db.String(255), nullable=True)
     data_cadastro = db.Column(db.DateTime, default=db.func.current_timestamp())
